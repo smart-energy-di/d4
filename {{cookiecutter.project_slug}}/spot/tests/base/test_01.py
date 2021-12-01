291427247cd87b1abfa063be6f6ae179460722c7
Markus Leist <markus@lei.st>
2021-12-01 18:04:45 +0100

    Remove keycloak from base tests

--- {{cookiecutter.project_slug}}/spot/tests/base/test_01.py
+++ {{cookiecutter.project_slug}}/spot/tests/base/test_01.py
@@ -26,19 +26,6 @@ def http_service(docker_ip, docker_services):
     return url
 
 
-@pytest.fixture(scope="session")
-def keycloak_service(docker_ip, docker_services):
-    """Ensure that HTTP service is up and responsive."""
-
-    # `port_for` takes a container port and returns the corresponding host port
-    port = docker_services.port_for("keycloak", 8080)
-    url = "http://{}:{}".format(docker_ip, port)
-    docker_services.wait_until_responsive(
-        timeout=60.0, pause=0.1, check=lambda: is_responsive(url)
-    )
-    return url
-
-
 def test_status_code(http_service):
     status = 200
     response = requests.get(http_service + "/")
@@ -50,10 +37,3 @@ def test_result_01(http_service):
     response = requests.get(http_service + "/")
 
     assert 'message' in response.json()
-
-
-def test_keycloak_status_code(keycloak_service):
-    status = 200
-    response = requests.get(keycloak_service + "/")
-
-    assert response.status_code == status

be65a631cb3450f1c020d8b8d7007a8ed087fbe8
Markus Leist <markus@lei.st>
2021-12-01 17:58:38 +0100

    split tests

---
+++ {{cookiecutter.project_slug}}/spot/tests/auth/test_01.py
@@ -0,0 +1,33 @@
+import pytest
+import requests
+
+from requests.exceptions import ConnectionError
+
+
+def is_responsive(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return True
+    except ConnectionError:
+        return False
+
+
+@pytest.fixture(scope="session")
+def keycloak_service(docker_ip, docker_services):
+    """Ensure that HTTP service is up and responsive."""
+
+    # `port_for` takes a container port and returns the corresponding host port
+    port = docker_services.port_for("keycloak", 8080)
+    url = "http://{}:{}".format(docker_ip, port)
+    docker_services.wait_until_responsive(
+        timeout=60.0, pause=0.1, check=lambda: is_responsive(url)
+    )
+    return url
+
+
+def test_keycloak_status_code(keycloak_service):
+    status = 200
+    response = requests.get(keycloak_service + "/")
+
+    assert response.status_code == status

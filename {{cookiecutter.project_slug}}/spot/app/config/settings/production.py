d1821b5e1627400061780c3c398b7ce1892bbec0
Markus Leist <markus@lei.st>
2024-02-12 22:55:03 +0100

    default value for env vars

--- {{cookiecutter.project_slug}}/spot/app/config/settings/production.py
+++ {{cookiecutter.project_slug}}/spot/app/config/settings/production.py
@@ -28,8 +28,8 @@ DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="").split(" ")
+CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", default="").split(" ")
 
 IMPORT_EXPORT_FORMATS = [XLSX, CSV, JSON]
 
